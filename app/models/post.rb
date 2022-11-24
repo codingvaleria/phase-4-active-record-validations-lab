8cb659dd58f6bca5ef91ca878e2034a44c3cd02e
codingvaleria <codingvaleria@gmail.com>
2022-11-24 18:22:49 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length:{ minimum: 250 }
+    validates :summary, length:{ maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate  :is_clickbaity
+
+    STRINGS = [/Secret/i, /Won't Believe/i, /Guess/i, /Top \d/i]
+    def is_clickbaity
+        if STRINGS.none? { |t| t.match title }
+         errors.add(:title, "must be clickbait")
+        end
+  end
+
+        
 end
